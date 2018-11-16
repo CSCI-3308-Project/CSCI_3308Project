f5f48240e571d08a59e0ba65007886099e822998
Justin Adkins <jadkins1019@gmail.com>
2018-11-16 16:23:43 -0700

    Adding postData file

---
+++ serverSide/db/seeds/postData.js
@@ -0,0 +1,122 @@
+export const posts = [
+  {
+    post_id: 1,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 2,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 3,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 4,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 5,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 6,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 7,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 8,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 9,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 10,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 11,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 12,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 13,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 14,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 15,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 16,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 17,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 18,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 19,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  },
+  {
+    post_id: 20,
+    course: "CSCI_2270",
+    location: "CSEL",
+    note: "I need help with red / black trees!"
+  }
+]
