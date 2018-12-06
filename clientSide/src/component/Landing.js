bb0bffa4cf9436fd6d84049979d4a3dfc13f6d6b
pbhetwal <pari.bhetwal@gmail.com>
2018-12-05 22:17:44 -0700

    Mission statement

--- clientSide/src/component/Landing.js
+++ clientSide/src/component/Landing.js
@@ -8,8 +8,13 @@ class Landing extends React.Component {
       <div className = "Landing">
         <img src={imgSrc} className="bg" alt="Study Buddies"/>
       	<div className = "Body">
-        	<h1> This page says some cool shit about us. Look at how cool we are!
+        	<h1> 
+            Study Buddies is dedicated to helping computer science students at CU Boulder. 
+            We know that many times it is difficult to find help with studying or finishing assignments. 
+            Through this application, you now have a platform where you can 
+            meet up with other students so that you can study and work together. 
         	</h1>
+          <h1 align="center"> Find your study buddy! </h1> 
       	</div>
       </div>
     )
