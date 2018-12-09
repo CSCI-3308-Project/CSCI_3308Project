702df803486de19135fcea13d9fc9e3ca1968993
Julien Rumsey <julienrumsey@cu-engr2-2-1-10.201.65.192.int.colorado.edu>
2018-12-09 14:45:12 -0700

    Adding testing modifications

--- clientSide/test.js
+++ clientSide/test.js
@@ -1,61 +1,66 @@
 var assert = require('assert'),
 http = require('http');
-const express = require('express');
-//const bcrypt = require('bcrypt');
-//var jwt = require('jsonwebtoken');
-//var exjwt = require('express-jwt');
-const router = express.Router();
-const User = require('../db/user_id');
 
+var expect = require('chai').expect;
+var request = require('request');
 
-
-describe('/', function () {
-
-  it('Front end is up', function (done) {
+describe('Server', function () {
+  it('Front end', function (done) {
     http.get('http://localhost:3000', function (res) {
       assert.equal(200, res.statusCode);
       done();
     });
-  });
+});
 
-  it('Back end is up', function (done) {
+  it('Back end', function (done) {
     http.get('http://localhost:8000', function (res) {
       assert.equal(200, res.statusCode);
       done();
     });
   });
+});
 
+describe ('Login page', function() {
+  it('status', function(done){
+    request('http://localhost:3000/login', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
 
-
-
-
-
-
-
-
-//it ('users register into database',function(done) {
-
-//assert.equal("shania@gmail.com",)
-
-
-//}
-
-
+describe ('Register page', function() {
+  it('status', function(done){
+    request('http://localhost:3000/register', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+describe ('Home page', function() {
+  it('status', function(done){
+    request('http://localhost:3000/home', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
 });
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Study Buddies Server', condition: true, anyArray: [1,2,3]});
-});
 
-router.get('/test/:id/', function(req, res, next) {
-  res.render('test', {output: req.params.id})
+describe ('Database is Populated', function() {
+  it('status', function(done){
+    request('http://localhost:8000/data', function(error, response, body) {
+      Object.size = function(body){
+        var size = 0, key;
+        for (key in body){
+          if (body.hasOwnProperty(key)) size++;
+        }
+        return size
+      };
+      var size = Object.size(body);
+      expect(body).to.have.lengthOf(size);
+      done();
+    });
+  });
 });
-
-router.post('/test/submit', function(req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
-});
-
-module.exports = router;
