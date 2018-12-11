c6c35cffe595d7b85df82de84fa02e2efdd83024
pbhetwal <pari.bhetwal@gmail.com>
2018-12-10 20:18:15 -0700

    bottom-nav-fix

--- clientSide/src/component/BotNav.js
+++ clientSide/src/component/BotNav.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link, withRouter } from "react-router-dom";
-import {NavLink, Fa, Row, Col } from "mdbreact";
+import React from 'react';
+import {withRouter} from "react-router-dom";
+import {Row, Col } from "mdbreact";
 
 import {Footer} from 'react-materialize';
 import './css/BotNav.css';
 
-var contact_link = "https://mail.google.com/mail/?view=cm&fs=1&to=" + "justin.adkins@colorado.edu";
-var help_link = "https://mail.google.com/mail/?view=cm&fs=1&to=" + "justin.adkins@colorado.edu";
-var about_link = "https://mail.google.com/mail/?view=cm&fs=1&to=" + "justin.adkins@colorado.edu";
+var about_React = "https://reactjs.org/tutorial/tutorial.html";
+var about_NodeJS = "https://nodejs.org/en/about/";
+var about_PSQL = "https://www.postgresql.org/about/";
 export class BotNav extends React.Component {
   render(){
 return (
@@ -21,15 +20,15 @@ return (
 
     <Row>
       <Col sm = '8'><p>&copy; 2018 Copyright Text</p></Col>
-      <Col sm='1'><a href={about_link} target="_blank" rel="noopener noreferrer">About Us</a></Col>
-      <Col sm ='1'><a href={help_link} target="_blank" rel="noopener noreferrer">Help</a></Col>
-      <Col sm = '1'><a href={contact_link} target="_blank" rel="noopener noreferrer">Contact Us</a></Col>
+      <Col sm='1'><a href={about_React} target="_blank" rel="noopener noreferrer">React</a></Col>
+      <Col sm ='1'><a href={about_NodeJS} target="_blank" rel="noopener noreferrer">Node.js</a></Col>
+      <Col sm = '1'><a href={about_PSQL} target="_blank" rel="noopener noreferrer">PostgreSQL</a></Col>
       
     </Row>
 
   }
 
-  className='example'
+    className='example'
   >
   </Footer>
 
